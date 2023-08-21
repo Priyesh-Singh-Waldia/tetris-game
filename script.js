16b125a25c876eeb7a00b690c2db4b36c0f00fa7
Priyesh Singh Waldia <114400261+Priyesh-Singh-Waldia@users.noreply.github.com>
2023-08-21 16:13:03 +0530

    project initiatted

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = document.querySelectorAll(".grid div");
+  const width = 10;
+});
